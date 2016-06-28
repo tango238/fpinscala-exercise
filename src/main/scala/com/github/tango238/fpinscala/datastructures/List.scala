f150e92c213f975637361a9c6648766ddd2437e6
Go Tanaka <tanago3@gmail.com>
2016-06-29 08:44:05 +0900

    added sum.

--- src/main/scala/com/github/tango238/fpinscala/datastructures/List.scala
+++ src/main/scala/com/github/tango238/fpinscala/datastructures/List.scala
@@ -6,8 +6,15 @@ case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
 
-  def sum(ints: List[Int]): Int = ???
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
 
   def product(ds: List[Double]): Double = ???
 
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail:_*))
 }
--- src/test/scala/com/github/tango238/fpinscala/datastructures/ListSpec.scala
+++ src/test/scala/com/github/tango238/fpinscala/datastructures/ListSpec.scala
@@ -1,2 +1,15 @@
 package com.github.tango238.fpinscala.datastructures
 
+import org.specs2._
+
+class ListSpec extends Specification { def is = s2"""
+
+ This is a specification to check the 'Hello world' string
+
+ The 'Hello world' string should
+   1 + 2 must be 3                              $e1
+  """
+
+  def e1 = List.sum(List(1, 2)) must_== 3
+
+}
