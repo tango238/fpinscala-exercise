6e0d36773abd3cafec9c2120d8b447ae93da87b9
Go Tanaka <tanago3@gmail.com>
2016-06-30 23:50:18 +0900

    added product and drop.

--- src/main/scala/com/github/tango238/fpinscala/datastructures/List.scala
+++ src/main/scala/com/github/tango238/fpinscala/datastructures/List.scala
@@ -6,15 +6,30 @@ case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
 
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
   def sum(ints: List[Int]): Int = ints match {
     case Nil => 0
     case Cons(x, xs) => x + sum(xs)
   }
 
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1
+    case Cons(x, xs) => x * product(xs)
+  }
 
-  def product(ds: List[Double]): Double = ???
+  // リストの先頭から n 個の要素を削除する
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n <= 0) l
+    else
+      l match {
+        case Cons(_, xs) => drop(xs, n - 1)
+        case Nil => Nil
+      }
 
-  def apply[A](as: A*): List[A] =
-    if (as.isEmpty) Nil
-    else Cons(as.head, apply(as.tail:_*))
+  // 述語とマッチする場合に限り、Listからその要素までの要素を削除する
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+
 }
--- src/test/scala/com/github/tango238/fpinscala/datastructures/ListSpec.scala
+++ src/test/scala/com/github/tango238/fpinscala/datastructures/ListSpec.scala
@@ -4,12 +4,26 @@ import org.specs2._
 
 class ListSpec extends Specification { def is = s2"""
 
- This is a specification to check the 'Hello world' string
-
- The 'Hello world' string should
-   1 + 2 must be 3                              $e1
+ The 'List' should
+   1 + 2 = 3                              $e1
+   2 * 3 = 6                              $e2
+   2 * 3 * 4   = 24                       $e3
+   2 * 3 * 4 * 0 = 0                      $e4
+   (2 + 3) * 4 = 20                       $e5
+   (2 + 3 + 0) * 4 = 20                   $e6
+   [1, 2, 3] drops 1 = [2, 3]             $e7
+   [1] drops 3 = Nil                      $e8
+   [0, 2, 4] filters by 'x > 0' = [2, 4]  $e9
   """
 
   def e1 = List.sum(List(1, 2)) must_== 3
-
+  def e2 = List.product(List(2, 3)) must_== 6
+  def e3 = List.product(List(2, 3, 4)) must_== 24
+  def e4 = List.product(List(2, 3, 4, 0)) must_== 0
+  def e5 = List.product(List(List.sum(List(2, 3)), 4)) must_== 20
+  def e6 = List.product(List(List.sum(List(2, 3, 0)), 4)) must_== 20
+  def e7 = List.drop(List(1, 2, 3), 1) must_== List(2, 3)
+  def e8 = List.drop(List(1), 3) must_== Nil
+  def gtZero(i: Int) = i > 0
+  def e9 = List.dropWhile(List(0, 2, 4), gtZero) must_== List(2, 4)
 }
