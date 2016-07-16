2225c76af8c9f82a23313d304d7508405b2c6a9f
Go Tanaka <tanago3@gmail.com>
2016-07-16 21:55:29 +0900

    added some methods and tests.

--- src/main/scala/com/github/tango238/fpinscala/errorhandling/Option.scala
+++ src/main/scala/com/github/tango238/fpinscala/errorhandling/Option.scala
@@ -2,27 +2,35 @@ package com.github.tango238.fpinscala.errorhandling
 
 sealed trait Option[+A] {
 
-  def map[B](f: A => B): Option[B] = {
-    this match {
-      case Some(a) => Some(f(a))
-      case None => None
-    }
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(a) => Some(f(a))
+    case None => None
   }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
-
-  def getOrElse[B >: A](default: => B):B = {
-    this match {
-      case Some(a) => a
-      case None => default
-    }
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case Some(a) => f(a)
+    case None => None
   }
 
-  def orElse[B >: A](ob: => Option[B]): Option[B] = ???
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(a) => a
+    case None => default
+  }
 
-  def filter(f: A => Boolean): Option[A] = ???
+  // Noneだったら、指定したデフォルトのOption型を返す。SomeだったらそのままそのSomeを返す
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
+    case Some(a) => this
+    case None => None
+  }
+
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(a) if f(a) => this
+    case _ => None
+  }
 
 }
+
 case class Some[+A](get: A) extends Option[A]
+
 case object None extends Option[Nothing]
 
--- src/test/scala/com/github/tango238/fpinscala/errorhandling/OptionSpec.scala
+++ src/test/scala/com/github/tango238/fpinscala/errorhandling/OptionSpec.scala
@@ -9,17 +9,19 @@ class OptionSpec extends Specification {
       Some(1).map(_ * 2) must_== Some(2)
     }
     "flatMap" in {
-      ok
+      Some(1).flatMap(a => Some(a + 1)) must_== Some(2)
     }
     "getOrElse" in {
       Some("a").getOrElse("b") must_== "a"
       None.getOrElse("b") must_== "b"
     }
     "orElse" in {
-      ok
+      Some("a").orElse(None) must_== Some("a")
+      None.orElse(None) must_== None
     }
     "filter" in {
-      ok
+      Some(10).filter(a => a > 0) must_== Some(10)
+      Some(10).filter(a => a > 10) must_== None
     }
   }
 }
