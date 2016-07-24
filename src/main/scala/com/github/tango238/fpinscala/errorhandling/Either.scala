a68fa3da81aaeaeb89e7551ba13ce262ce9f3a34
Go Tanaka <tanago3@gmail.com>
2016-07-24 21:02:37 +0900

    add map, flatMap, orElse, map2

--- src/main/scala/com/github/tango238/fpinscala/errorhandling/Either.scala
+++ src/main/scala/com/github/tango238/fpinscala/errorhandling/Either.scala
@@ -7,6 +7,15 @@ sealed trait Either[+E, +A] {
       Left("mean of empty list")
     else
       Right(xs.sum / xs.length)
+
+  def map[B](f: A => B): Either[E, B] = ???
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+
+  def orElse[EE >: E,B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A,B) => C): Either[EE, C] = ???
+
 }
 
 case class Left[+E](value: E) extends Either[E, Nothing]
