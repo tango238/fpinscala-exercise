f72cfebed4fcdfc4e0ebd582c83c8e3cfe96476a
Go Tanaka <tanago3@gmail.com>
2016-08-04 22:30:26 +0900

    implement Functor.

---
+++ src/main/scala/com/github/tango238/fpinscala/monads/Monad.scala
@@ -0,0 +1,17 @@
+package com.github.tango238.fpinscala.monads
+
+trait Functor[F[_]] {
+  def map[A, B](fa: F[A])(f: A ⇒ B): F[B]
+
+  def distribute[A, B](fab: F[(A, B)]): (F[A], F[B]) = (map(fab)(_._1), map(fab)(_._2))
+
+  def codistribute[A, B](e: Either[F[A], F[B]]): F[Either[A, B]] = e match {
+    case Left(fa) ⇒ map(fa)(Left(_))
+    case Right(fb) ⇒ map(fb)(Right(_))
+  }
+
+  val listFunctor = new Functor[List] {
+    def map[A, B](as: List[A])(f: A ⇒ B): List[B] = as map f
+  }
+}
+
